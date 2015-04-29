ffffe91a9ae4c9552c935cf8e4cee78fe5414789
Francesco Fraccaroli <francesco.fraccaroli@lever.it>
2015-04-29 11:58:46 +0200

    Added url/IP autorecognising

--- ntptest.cpp
+++ ntptest.cpp
@@ -1,63 +1,120 @@
 #include "ntptest.h"
 #include <QDebug>
+#include <QCoreApplication>
+#include <QStringList>
 #include <sys/time.h>
 
-#define CET (+1)
-#define DST ()
+#define DEBUG_LEVEL 2   //0=no, 1=to qDebug(), 2=to log file and qDebug()
+
+#if DEBUG_LEVEL==0
+    #define debugC(x) ;     //info
+#endif
+
+#if DEBUG_LEVEL==1
+    #define debugC(x) qDebug() << x   //info
+#endif
+
+#if DEBUG_LEVEL==2
+#include <QFile>
+#define debug_path "/local/root/log/logNtp.txt"
+#define debugC(x) {QFile fileLog(debug_path); fileLog.open(QIODevice::Append | QIODevice::Text); QTextStream(&fileLog) << x << "\n"; qDebug() << x;}
+#endif
 
 NtpTest::NtpTest(int timeZone, QObject *parent) :
     QObject(parent),
-    m_timeZone(timeZone*3600)
+    m_timeZone(timeZone*3600),
+    m_timer(0)
 {
+    debugC("start Npt");
     m_client = new NtpClient(this);
     connect(m_client, SIGNAL(replyReceived(QHostAddress,quint16,NtpReply)), this, SLOT(onReplyReceived(QHostAddress,quint16,NtpReply)));
+    m_timer = new QTimer();
+    m_timer->setInterval(15000);
+    connect(m_timer, SIGNAL(timeout()), this, SLOT(timeEnd()));
+}
+
+void NtpTest::timeEnd(){
+    debugC("Terminated for timeout.");
+    qApp->quit();
 }
 
 void NtpTest::run()
 {
     // tok-ntp-ext.asi 17.82.254.14     2 u 1141  512    1  571.869  -735.58  45.102
     m_client->sendRequest(QHostAddress("17.82.254.14"), 123);
+    m_timer->start();
 }
 
 void NtpTest::run(QString servName)
 {
-    QHostInfo::lookupHost(servName, this, SLOT(lookedUp(QHostInfo)));
+    bool isIP;
+    //check if the servName is an IP address
+    QStringList campi = servName.split(".", QString::SkipEmptyParts);
+    if (campi.length()!=4)
+        isIP = false;
+    if (!((campi.at(0).toInt(&isIP)>=0) && (campi.at(0).toInt(&isIP)<=255)))
+        isIP = false;
+    if (!((campi.at(1).toInt(&isIP)>=0) && (campi.at(1).toInt(&isIP)<=255)))
+        isIP = false;
+    if (!((campi.at(2).toInt(&isIP)>=0) && (campi.at(2).toInt(&isIP)<=255)))
+        isIP = false;
+    if (!((campi.at(3).toInt(&isIP)>=0) && (campi.at(3).toInt(&isIP)<=255)))
+        isIP = false;
+
+    if (isIP){
+        m_client->sendRequest(QHostAddress(servName), 123);
+        debugC("Send request to IP " << servName);
+    }
+    else{
+        QHostInfo::lookupHost(servName, this, SLOT(lookedUp(QHostInfo)));
+        debugC("Lookup host for url " << servName);
+    }
+    m_timer->start();
 }
 
 void NtpTest::onReplyReceived(QHostAddress host, quint16 port, NtpReply reply)
 {
+    Q_UNUSED(host)
+    Q_UNUSED(port)
+
+    m_timer->stop();
     updateTime(reply.transmitTime().toTime_t());
+    debugC("Updated" << reply.transmitTime().toString());
+
+    qApp->quit();
 }
 
 void NtpTest::lookedUp(const QHostInfo &host)
 {
+    m_timer->stop();
     if (host.error() != QHostInfo::NoError) {
-        //qDebug() << "Cannot resolve URL:" << host.errorString();
-        return;
+        debugC("Cannot resolve URL:" << host.errorString());
+        qApp->quit();
     }
 
     foreach (const QHostAddress &address, host.addresses()){
-        //qDebug() << "Found address:" << address.toString();
+        debugC("Found address:" << address.toString());
         m_client->sendRequest(address, 123);
+        m_timer->start();
     }
 }
 
-void NtpTest::updateTime(time_t ora_s){
-    if (isDST(ora_s))
-        ora_s += 3600;  //if DST, add one hour.
+void NtpTest::updateTime(time_t hour_s){
+    if (isDST(hour_s))
+        hour_s += 3600;  //if DST, add one hour.
 
-    ora_s +=m_timeZone;
+    hour_s +=m_timeZone;
 
     struct timeval temp;
-    temp.tv_sec = ora_s;
+    temp.tv_sec = hour_s;
     temp.tv_usec = 0;
     settimeofday(&temp, 0);
     system("/sbin/hwclock -wu");        /* Update RTC from system */
 }
 
 //european DST: from the last sunday of March to the last sunday of October
-bool NtpTest::isDST(time_t ora_s){
-    struct tm *pt = gmtime(&ora_s);
+bool NtpTest::isDST(time_t hour_s){
+    struct tm *pt = gmtime(&hour_s);
 
     if (pt->tm_mon == MAR){
         pt->tm_mday = 31;   //set on April, 31th
@@ -67,7 +124,7 @@ bool NtpTest::isDST(time_t ora_s){
         pt->tm_hour = 2;
         pt->tm_min = 0;
         pt->tm_sec = 0;
-        if (ora_s >= mktime(pt))   //if greater (or equal), DST
+        if (hour_s >= mktime(pt))   //if greater (or equal), DST
             return true;
         else
             return false;
@@ -81,7 +138,7 @@ bool NtpTest::isDST(time_t ora_s){
         pt->tm_hour = 3;
         pt->tm_min = 0;
         pt->tm_sec = 0;
-        if (ora_s < mktime(pt))   //if lower, DST
+        if (hour_s < mktime(pt))   //if lower, DST
             return true;
         else
             return false;
--- ntptest.h
+++ ntptest.h
@@ -4,6 +4,7 @@
 #include <QObject>
 #include <QHostAddress>
 #include <QHostInfo>
+#include <QTimer>
 #include "qntp/QNtp.h"
 #include "qntp/NtpClient.h"
 #include "qntp/NtpReply.h"
@@ -13,7 +14,7 @@ class NtpTest : public QObject
     Q_OBJECT
 public:
 
-    //monts of the year
+    //monts of the year (in struct tm data)
     enum MONTHS{
         JAN=0,
         FEB=1,
@@ -38,6 +39,9 @@ public:
 
     /**
      * @brief Ask for a server reply. Resolve the url and then send a request.
+     * If the url is an IP address, send a request directly. Resolve
+     * an IP could be possibile, but may get problems in intranet without
+     * DNS servers.
      * @param servName Name of the NTP server (both IP or url)
      */
     void run(QString servName);
@@ -47,27 +51,43 @@ public:
      */
     void run();
 
+public slots:
+    void timeEnd();
+
 signals:
 
 private slots:
+    /**
+     * @brief Receive the timeserver responce and set the local time.
+     * @param host Address of the server
+     * @param port Port of the server
+     * @param reply Reply of the server
+     */
     void onReplyReceived(QHostAddress host, quint16 port, NtpReply reply);
+
+    /**
+     * @brief Receive the list of IP from DNS sever and send requests.
+     * If empty list, just quit.
+     * @param host Reply of the DSN server.
+     */
     void lookedUp(const QHostInfo &host);
 
     /**
      * @brief Update the system time. Works on Linux Embedded.
-     * @param ora_s Time to be set (UTC)
+     * @param hour_s Time to be set (UTC)
      */
-    void updateTime(time_t ora_s);
+    void updateTime(time_t hour_s);
 
     /**
      * @brief Calculate if the DST is active
-     * @param ora_s Time to calculate if the DST is applied or not.
+     * @param hour_s Time to calculate if the DST is applied or not.
      * @return true if DST has to be applied, false otherwise.
      */
-    bool isDST(time_t ora_s);
+    bool isDST(time_t hour_s);
 private:
-    NtpClient * m_client;
-    int m_timeZone;
+    NtpClient * m_client;   //!< Client fot NTP comms
+    int m_timeZone;     //!< Timezone
+    QTimer *m_timer;    //!< Timeout timer
 };
 
 #endif // NTPTEST_H
